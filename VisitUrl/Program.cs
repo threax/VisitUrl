76ff071e615bed3225d36a31640aaac1c50c69ba
Andrew Piper <piper.andrew@spcollege.edu>
2018-01-04 15:17:48 -0500

    Made program visit url passed on command line.

--- VisitUrl/Program.cs
+++ VisitUrl/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace VisitUrl
 {
@@ -6,7 +8,30 @@ namespace VisitUrl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 1)
+            {
+                Console.WriteLine("You must include the url to access as the first argument.");
+                return;
+            }
+
+            Task.Run(async () => await AsyncMain(args)).GetAwaiter().GetResult();
+        }
+
+        private static async Task AsyncMain(string[] args)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = httpClient.GetAsync(args[0]))
+                {
+                    using (var response = request.Result)
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Console.Write(await response.Content.ReadAsStringAsync());
+                        }
+                    }
+                }
+            }
         }
     }
 }
